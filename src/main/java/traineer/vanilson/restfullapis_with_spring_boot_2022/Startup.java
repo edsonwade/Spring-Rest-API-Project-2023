ec0da3a36042e51c14064e2d4eca10faf18ff910
vanil <vanil@DESKTOP-EPO41LJ>
2022-07-09 15:51:54 +0100

    intro

---
+++ src/main/java/traineer/vanilson/restfullapis_with_spring_boot_2022/Startup.java
@@ -0,0 +1,13 @@
+package traineer.vanilson.restfullapis_with_spring_boot_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
+
+}
---
+++ src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/StartupTests.java
@@ -0,0 +1,13 @@
+package traineer.vanilson.restfullapis_with_spring_boot_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
