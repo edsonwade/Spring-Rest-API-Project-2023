2381d8406af6efe501ffadb9062a420b2ab08395
vanil <vanil@DESKTOP-EPO41LJ>
2022-07-11 21:31:37 +0100

    mock teste service methods findById()

--- src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/StartupTests.java
+++ src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/StartupTests.java
@@ -1,9 +1,8 @@
 package traineer.vanilson.restfullapis_with_spring_boot_2022;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StartupTests {
 
     @Test
--- src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/persistence/model/Gender.java
+++
@@ -1,7 +0,0 @@
-package traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
