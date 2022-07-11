6258e139f0726202bed43881312dc962971123a5
vanil <vanil@DESKTOP-EPO41LJ>
2022-07-11 22:10:36 +0100

    update main

--- src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/mocks/MockPerson.java
+++ src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/mocks/MockPerson.java
@@ -1,35 +1,36 @@
-package traineer.vanilson.restfullapis_with_spring_boot_2022.mocks;
-
-import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Gender;
-import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Person;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MockPerson {
-
-    List<Person> people = new ArrayList<>();
-
-    public Person mockEntity() {
-        return mockEntity(0);
-    }
-
-    public List<Person> mockEntityList() {
-
-        for (int i = 0; i < 14; i++) {
-            people.add(mockEntity(i));
-        }
-        return people;
-    }
-
-    public Person mockEntity(Integer number) {
-        Person person = new Person();
-        person.setPerson_id(number);
-        person.setFirstName("First Name Test" + number);
-        person.setLastName("Last Name Test" + number);
-        person.setEmail("mock@email.test" + number);
-        person.setAddress("Address Test" + number);
-        person.setGender(((number % 2) == 0) ? Gender.MALE : Gender.FEMALE);
-        return person;
-    }
-}
+//package traineer.vanilson.restfullapis_with_spring_boot_2022.mocks;
+//
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Gender;
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Person;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class MockPerson {
+//
+//    List<Person> people = new ArrayList<>();
+//
+//    public Person mockEntity() {
+//        return mockEntity(0);
+//    }
+//
+//    public List<Person> mockEntityList() {
+//
+//        for (int i = 0; i < 14; i++) {
+//            people.add(mockEntity(i));
+//        }
+//        return people;
+//    }
+//
+//    public Person mockEntity(Integer number) {
+//        Person person = new Person();
+//        person.setPerson_id(number);
+//        person.setFirstName("First Name Test" + number);
+//        person.setLastName("Last Name Test" + number);
+//        person.setEmail("mock@email.test" + number);
+//        person.setAddress("Address Test" + number);
+//        person.setGender(((number % 2) == 0) ? Gender.MALE : Gender.FEMALE);
+//        return person;
+//    }
+//}
+//
--- src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/services/PersonServiceImplTest.java
+++ src/test/java/traineer/vanilson/restfullapis_with_spring_boot_2022/services/PersonServiceImplTest.java
@@ -1,66 +1,65 @@
-package traineer.vanilson.restfullapis_with_spring_boot_2022.services;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-import traineer.vanilson.restfullapis_with_spring_boot_2022.mocks.MockPerson;
-import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Gender;
-import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Person;
-import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.repository.PersonRepository;
-
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-@ExtendWith(MockitoExtension.class)
+//package traineer.vanilson.restfullapis_with_spring_boot_2022.services;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestInstance;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.MockitoAnnotations;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.mocks.MockPerson;
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Gender;
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.model.Person;
+//import traineer.vanilson.restfullapis_with_spring_boot_2022.persistence.repository.PersonRepository;
+//
+//import java.util.Optional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//@ExtendWith(MockitoExtension.class)
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class PersonServiceImplTest {
-
-    private final PersonRepository personRepository = mock(PersonRepository.class);
-    private final PersonServiceImpl personService = new PersonServiceImpl(personRepository);
-
-    MockPerson person;
-
-//    @InjectMocks
-//    private PersonServiceImpl personService;
-
-    @BeforeEach
-    void setUp() {
-        person = new MockPerson();
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    void findAllPersonsExistentInDatabase() {
-    }
-
-    @Test
-    void findPersonByIdAndReturnThePerson() {
-        Person person1 = person.mockEntity(1);
-        person1.setPerson_id(1);
-        when(personRepository.findById(1)).thenReturn(Optional.of(person1));
-
-        Person person = personService.findById(1);
-
-        assertNotNull(person);
-        assertNotNull(person.getPerson_id());
-        assertNotNull(person.getLinks());
-        person.toString();
-        assertTrue(true);
-        assertEquals("Address Test1", person.getAddress());
-        assertEquals("mock@email.test1", person.getEmail());
-        assertEquals("First Name Test1", person.getFirstName());
-        assertEquals("Last Name Test1", person.getLastName());
-        assertEquals(Gender.FEMALE, person.getGender());
-
-
-    }
-
-    @Test
-    void createNewPerson() {
-    }
-}
+//class PersonServiceImplTest {
+//
+//    private final PersonRepository personRepository = mock(PersonRepository.class);
+//    private final PersonServiceImpl personService = new PersonServiceImpl(personRepository);
+//
+//    MockPerson person;
+//
+//@InjectMocks
+//private PersonServiceImpl personService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        person = new MockPerson();
+//        MockitoAnnotations.openMocks(this);
+//    }
+//
+//    @Test
+//    void findAllPersonsExistentInDatabase() {
+//    }
+//
+//    @Test
+//    void findPersonByIdAndReturnThePerson() {
+//        Person person1 = person.mockEntity(1);
+//        person1.setPerson_id(1);
+//        when(personRepository.findById(1)).thenReturn(Optional.of(person1));
+//
+//        Person person = personService.findById(1);
+//
+//        assertNotNull(person);
+//        assertNotNull(person.getPerson_id());
+//        assertNotNull(person.getLinks());
+//        assertEquals("Address Test1", person.getAddress());
+//        assertEquals("mock@email.test1", person.getEmail());
+//        assertEquals("First Name Test1", person.getFirstName());
+//        assertEquals("Last Name Test1", person.getLastName());
+//        assertEquals(Gender.FEMALE, person.getGender());
+//
+//
+//    }
+//
+//    @Test
+//    void createNewPerson() {
+//    }
+//        }
