3daf9b22c1829e8b3a2220b1cb62e7a287cfd449
vanil <vanil@DESKTOP-EPO41LJ>
2022-07-11 14:44:27 +0100

    application json or xml in header accept

--- src/main/java/traineer/vanilson/restfullapis_with_spring_boot_2022/configuration/WebConfig.java
+++ src/main/java/traineer/vanilson/restfullapis_with_spring_boot_2022/configuration/WebConfig.java
@@ -11,9 +11,19 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         // via query param http://localhost:8080//api/v1/person?mediaType=xml
 
-        configurer.favorParameter(true)
+        /*configurer.favorParameter(true)
                 .parameterName("mediaType")
                 .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+        // via header param http://localhost:8080//api/v1/person
+        // change header in postman with Accept (key) -Application/xml or json value
+
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
