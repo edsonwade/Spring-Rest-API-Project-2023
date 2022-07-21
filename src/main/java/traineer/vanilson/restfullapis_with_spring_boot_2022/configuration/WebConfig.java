3c44838065fb0aeab28b4bdaee626e7c1efc8885
vanil <vanil@DESKTOP-EPO41LJ>
2022-07-21 12:39:06 +0100

    cors configuration not working need to be fixed

--- src/main/java/traineer/vanilson/restfullapis_with_spring_boot_2022/configuration/WebConfig.java
+++ src/main/java/traineer/vanilson/restfullapis_with_spring_boot_2022/configuration/WebConfig.java
@@ -1,9 +1,11 @@
 package traineer.vanilson.restfullapis_with_spring_boot_2022.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import traineer.vanilson.restfullapis_with_spring_boot_2022.serialization_converter.YamlJacksonTwoHttpMessageConverter;
 
@@ -13,6 +15,9 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
 
+
+    @Value("${cors.originPatterns:default")
+    private String corsOriginPatters = "";
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -41,5 +46,17 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new YamlJacksonTwoHttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatters.split(",");
+
+        registry.addMapping("/**")
+                //.allowedMethods("GET","POST","PUT")
+                .allowedMethods("*")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
+
     }
 }
